b097d39f3a109e406d9c01e4bab962a53ffa6d8b
Sanjai kumar <119435129+sanjai-AK47@users.noreply.github.com>
2023-12-28 21:52:46 +0530

    Update1.0.1 setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='probuster',
-    version='1.0.0',
+    version='1.0.1',
     author='D. Sanjai Kumar',
     author_email='bughunterz0047@gmail.com',
     description=' A Python based Web Application Penetration testing tool for Information Gathering.',
@@ -16,7 +16,11 @@ setup(
         'Requests>=2.31.0',
         'rich>=13.7.0',
         'urllib3>=1.26.18',
-        'streamlit>=1.29.0'
+        'streamlit>=1.29.0',
+        'aiofiles>=23.2.1',
+        'aiohttp>=3.8.6',
+        'asyncio>=3.4.3'
+        
         
     ],
     entry_points={
